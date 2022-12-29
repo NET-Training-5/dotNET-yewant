17f047bbd13840c42a18620f3342b6ea1d28cb95
yewantkarki <kyewantraj19@tbc.edu.np>
2022-12-29 21:15:05 +0545

    employee details with model

--- Web Development/HelloWeb/Controllers/EmployeeController.cs
+++ Web Development/HelloWeb/Controllers/EmployeeController.cs
@@ -5,6 +5,36 @@ public class EmployeeController : Controller
 {
     public IActionResult EmpDetails()       //  /number/greet
     {
-        return View();
+        Employee e1 = new Employee()
+        {
+            FullName = "Yewant Karki",
+            Sex = Gender.Male,
+            JoinDate = new DateTime(2010, 7, 12),
+            Designation = "Software Engineer",
+            Department = "Research and Development"
+        };
+
+        Employee e2 = new Employee()
+        {
+            FullName = "Nipson K.C.",
+            Sex = Gender.Male,
+            JoinDate = new DateTime(2009, 6, 10),
+            Designation = "Sr. Software Engineer",
+            Department = "Research and Development"
+        };
+
+        Employee e3 = new Employee()
+        {
+            FullName = "Susan Paudel",
+            Sex = Gender.Female,
+            JoinDate = new DateTime(2013, 5, 10),
+            Designation = "Sr. Software Engineer",
+            Department = "Operations"
+        };
+
+        List<Employee> employees = new List<Employee>() { e1, e2, e3 };
+
+        return View(employees);
     }
 }
+
---
+++ Web Development/HelloWeb/Models/Employee.cs
@@ -0,0 +1,8 @@
+public class Employee
+{
+    public string FullName { get; set; }
+    public Gender Sex { get; set; }
+    public DateTime JoinDate { get; set; }
+    public string Designation { get; set; }
+    public string Department { get; set; }
+}
